5b6a5a7f91bc71abf10d486875d177e9343ba7e5
Lucas Mazza <lucastmazza@gmail.com>
2014-07-07 14:10:55 -0300

    Move sources around, load ~/.secrets.sh if present

--- init.sh
+++ init.sh
@@ -8,12 +8,13 @@ if [ -f `brew --prefix`/etc/bash_completion ]; then
   . `brew --prefix`/etc/bash_completion
 fi
 
-source $ROOT/scripts/prompt.sh
-source $ROOT/scripts/aliases.sh
-
 export PATH="$HOME/bin:$ROOT/bin:$PATH"
 export EDITOR="atom -w -n"
 
 export BUNDLE_JOBS=1
 
 export ATOM_REPOS_HOME="$HOME/code/github"
+
+test -f ~/.secrets.sh && source ~/.secrets.sh
+source $ROOT/scripts/prompt.sh
+source $ROOT/scripts/aliases.sh
