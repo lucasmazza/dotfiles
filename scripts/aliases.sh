7de9f210782b8a2ac2ffe00f14d8c0f352496615
Lucas Mazza <lucastmazza@gmail.com>
2014-07-11 14:52:45 -0300

    Desktop on/off can be just two aliases.

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -4,8 +4,11 @@ alias ls='ls -AlhF'
 alias squid='squid -N -d 1'
 alias ia='open $1 -a /Applications/iA\ Writer.app'
 alias ql='qlmanage -p "$@" >& /dev/null'
-alias spoton='sudo mdutil -a -i on'
-alias spotoff='sudo mdutil -a -i off'
+
+alias spotlighton='sudo mdutil -a -i on'
+alias spotlightoff='sudo mdutil -a -i off'
+alias desktopon='defaults write com.apple.finder CreateDesktop -bool true && killall Finder'
+alias desktopoff='defaults write com.apple.finder CreateDesktop -bool false && killall Finder'
 
 alias rtest='bundle exec ruby -Ilib:test "$@"'
 alias rserver='ruby -run -e httpd -- -p 9292 .'
