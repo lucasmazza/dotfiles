88825034920ee71ffe8451b66a00233b2145d532
Lucas Mazza <git@lucasmazza.website>
2021-08-25 16:28:03 -0300

    Add `.iex.exs` symlink

---
+++ symlinks/iex.exs
@@ -0,0 +1,16 @@
+defmodule IEx.Custom do
+  def pbcopy(term) do
+    text =
+      if is_binary(term) do
+        term
+      else
+        inspect(term, limit: :infinity, pretty: true)
+      end
+
+    port = Port.open({:spawn, "pbcopy"}, [])
+    true = Port.command(port, text)
+    true = Port.close(port)
+
+    :ok
+  end
+end
