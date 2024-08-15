e20bc3b92db36fa1c9c5c87def9891558aad7e36
Lucas Mazza <git@lucasmazza.website>
2024-08-15 19:24:24 -0300

    add `notes` alias

--- aliases.sh
+++ aliases.sh
@@ -5,3 +5,5 @@ alias spotlighton='sudo mdutil -a -i on'
 alias spotlightoff='sudo mdutil -a -i off'
 alias desktopon='defaults write com.apple.finder CreateDesktop -bool true && killall Finder'
 alias desktopoff='defaults write com.apple.finder CreateDesktop -bool false && killall Finder'
+
+alias notes='code -n ~/Desktop/notes.md'
