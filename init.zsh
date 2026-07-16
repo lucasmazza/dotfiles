949a687560bd2ed57cecc47ac6df12531143255f
Lucas Mazza <git@lucasmazza.website>
2026-07-16 19:19:37 -0300

    use `zed` by default

--- init.zsh
+++ init.zsh
@@ -14,7 +14,7 @@ for directory in $HOME/src/*; do
 done
 
 export PATH="$HOME/bin:$HOME/.bin:$root/bin:$PATH"
-export EDITOR="code -w -n"
+export EDITOR="zed -w -n"
 
 # Set `mise/asdf install` flags for Erlang and Postgres
 export KERL_CONFIGURE_OPTIONS="--disable-debug --without-javac"
